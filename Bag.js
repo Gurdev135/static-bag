374418f024dd34b6e0e564eac7621cf3e4d6fb5e
Gurdev Gill <108082625+Gurdev135@users.noreply.github.com>
2023-02-15 11:58:45 +0000

    Update Bag.js

--- Bag.js
+++ Bag.js
@@ -1,8 +1,12 @@
 class Bag {
+    static maxweight = 34;
     constructor(weight) {
         if(!weight){
             throw new Error('bag needs weight')
         } 
+        if(weight>Bag.maxweight){
+            throw new Error('exceeded maximum weight')
+        }
         this.weight = weight
     }
 }
